141d584a64b7935b13561c76e8904e6b17fb3e13
mani473-dev <nagamanikantamaganti7@gmail.com>
2026-08-10 17:09:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 from main import create_teams_meeting
 
@@ -8,9 +9,11 @@ app = FastAPI()
 
 class TeamsMeetingRequest(BaseModel):
     candidateName: str
+    email: str
     startDateTime: str
     endDateTime: str
     subject: str
+    interviewers: List[str]
 
 
 @app.post("/create-teams-meeting")
@@ -18,7 +21,9 @@ def execute(request: TeamsMeetingRequest):
 
     return create_teams_meeting(
         request.candidateName,
+        request.email,
         request.startDateTime,
         request.endDateTime,
-        request.subject
+        request.subject,
+        request.interviewers
     )
